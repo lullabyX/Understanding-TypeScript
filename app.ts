99ab36becebeb9831d7acd184a42611b812cec61
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-06 23:13:14 +0600

    tuple type

--- app.ts
+++ app.ts
@@ -1,20 +1,18 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobies: string[];
+  roles: [number, string]
+} = {
 // const person = { // infered if specific object type is not assigned
   name: 'Rabbi',
   age: 25,
-  hobies: ['Gaming', 'Reading Books', 32]
+  hobies: ['Gaming', 'Reading Books'],
+  roles: [2, 'author']
 }
 
-let favoriteActivities: string[];
-favoriteActivities = ['Gaming']
-
-for (const hobby in person.hobies)
-{
-  console.log(hobby.toUpperCase); // here typescript's type inferece detect hoppy is a type of string
-                                  // and suggest string methods for auto completion
-  
-  }
-
+// person.roles.push('Engineer') // !!! Exception: be mindful it works !!!
+// person.roles[1] = 35 !! ERROR !!
 console.log('====================================');
-console.log(person.name);
+console.log(person.roles);
 console.log('====================================');
