034e009ed815641aab834878cc1d0ad40c5bd82f
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-06 16:46:55 +0600

    Type assignment vs Type inference

--- app.ts
+++ app.ts
@@ -10,8 +10,10 @@ function add(n1: number, n2: number, printResult: boolean, prefix: string)
     return result
 }
 
-const number1 = 3
-const number2 = 4.5
+let number1 = 3 // type inference, ts infers number1 is type of 'number'
+const number2 = 4.5 // type inference, ts infers number2 is type of 4.5, since number2 is constant and can't change
 const printResult = true
+let aValue: number // type assignment
+aValue = 10
 
 add(number1, number2, printResult, 'Result is: ');
