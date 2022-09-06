76bf6eb2550db7b4cb6773f02cb5cbb226ca9580
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-06 16:04:55 +0600

    working with Numbers, Strings & Booleans

--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
-console.log('====================================');
-console.log('Trying out typescript');
-console.log('====================================');
+function add(n1: number, n2: number, printResult: boolean, prefix: string)
+{
+  const result = n1 + n2;
+  if (printResult)
+  {
+    console.log('====================================');
+    console.log(prefix + result);
+    console.log('====================================');
+  } else 
+    return result
+}
+
+const number1 = 3
+const number2 = 4.5
+const printResult = true
+
+add(number1, number2, printResult, 'Result is: ');
