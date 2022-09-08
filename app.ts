785dac7388d39902bf5d8b24187d5655f9fe7591
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-08 14:54:24 +0600

    unknown type, never type

--- app.ts
+++ app.ts
@@ -1,47 +1,21 @@
-type combinable = "add-text" | "add-num";
+let janiNa: unknown;
 
-function combine(
-  input1: string | number,
-  input2: string | number,
-  conversion: combinable
-) {
-  let result: string | number;
+let str: string;
 
-  // result = input1 + input2 // will throw error because TS knows inputs are union type
-  // but doesn't know what exact type it is and if it can be
-  // added together
-  if (
-    typeof input1 === "number" &&
-    typeof input2 === "number" &&
-    conversion !== "add-text"
-  ) {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
+str = "Biday Prithibi"
+janiNa = 'Hello World'
+
+// str = janiNa; // can't assign unknown type variable to a known type variable without type checking
+if (typeof janiNa === 'string') 
+  str = janiNa
+janiNa = str; // can assign known type variable to unknown type variable;
+
+function exception(str: string): never { // a function's return type never does not have reachable end point
+  throw {
+    'Error Message': str
   }
-
-  return result;
 }
-
-let addNumber: (a: number, b: number, cb: (c: number) => void) => void;
-
-function add(num1: number, num2: number, cb: (a: number) => void) {
-  const result = num1 + num2;
-  cb(result);
-}
-
-function printResult(result: number) {
-  console.log(result)
-}
-
-addNumber = add;
-
-addNumber(30, 34, printResult)
-
-const ageCombine = combine(20, 30, "add-text");
-const nameCombine = combine("Hassan", "Rabbi", "add-text");
 
 console.log("====================================");
-console.log(ageCombine);
-console.log(nameCombine);
+exception('An error has been thrown')
 console.log("====================================");
