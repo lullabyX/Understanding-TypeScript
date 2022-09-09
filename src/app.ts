dec38308b643a5194623f9bbf49337fc40ab2f58
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-09 21:03:25 +0600

    class and access modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
-console.log('====================================');
-console.log('Trying out typescript');
-console.log('====================================');
+class Department {
+  name: string
+  private employees: string[] = []
+
+  constructor (n: string) {
+    this.name = n
+  }
+
+  addEmployee(this: Department, ...employee: string[]) { // this in the parameter is not required, but if inserted
+                                                    // the thing calling the method must have the properties
+                                                    // this has
+    this.employees.push(...employee)
+    
+  }
+
+  showDetail(this: Department) {
+    console.log('Department: ' + this.name);
+    console.log('Number of employees: ' + this.employees.length);
+    console.log('Employees: '+ this.employees)
+  }
+}
+
+const accounting = new Department('Accounting');
+
+accounting.showDetail()
+
+accounting.addEmployee('Jerry', 'Tom')
+
+accounting.showDetail()
+
+
+const something = {addEmployee: accounting.addEmployee}
+
+// something.addEmployee('Jerry') // throws error because of 'this' in param, something doesn't have employee proterty
+// console.log(accounting.employees); // throws error because 'employees' is private
