b1c19f2d83fc34564f26378eaf5dcbf94eef011c
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-10 17:25:34 +0600

    overriding properties and protected modifier

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,75 @@
 class Department {
-  name: string
-  private employees: string[] = []
+  name: string;
+  protected employees: string[] = [];
 
-  constructor (public readonly id: string, n: string) {
-    this.name = n
+  constructor(public readonly id: string, n: string) {
+    this.name = n;
   }
 
-  addEmployee(this: Department, ...employee: string[]) { // this in the parameter is not required, but if inserted
-                                                    // the thing calling the method must have the properties
-                                                    // this has
-    this.employees.push(...employee)
-    
+  addEmployee(this: Department, ...employee: string[]) {
+    // this in the parameter is not required, but if inserted
+    // the thing calling the method must have the properties
+    // this has
+    this.employees.push(...employee);
   }
 
   showDetail(this: Department) {
-    console.log('Department: ' + this.name);
-    console.log('Number of employees: ' + this.employees.length);
-    console.log('Employees: '+ this.employees)
+    console.log("Department: " + this.name);
+    console.log("Number of employees: " + this.employees.length);
+    console.log("Employees: " + this.employees);
   }
 }
 
 class ITDepartment extends Department {
   // I don't need to declare attributes if I use shortcut
-  constructor (id: string, public admins: string[]) { // if you declare access modifier(public/private), you
-                                                      // can have shortcut where you don't need to declare
-                                                      // and assign this.that = that
-    super(id, 'IT');
+  constructor(id: string, public admins: string[]) {
+    // if you declare access modifier(public/private), you
+    // can have shortcut where you don't need to declare
+    // and assign this.that = that
+    super(id, "IT");
     // here I don't need to assign this.admins = admins because of above reason
   }
 }
 
-const it = new ITDepartment('d1', ['Max', 'Hassan']);
+const it = new ITDepartment("d1", ["Max", "Hassan"]);
 
-it.showDetail()
+it.showDetail();
 
-it.addEmployee('Jerry', 'Tom')
+it.addEmployee("Jerry", "Tom");
 
-it.showDetail()
+it.showDetail();
 
 class AccountingDepartment extends Department {
-  private reports: string[] = []
-  constructor (id: string) {
-    super(id, 'Accounting');
+  private reports: string[] = [];
+  constructor(id: string) {
+    super(id, "Accounting");
+  }
+
+  addEmployee(this: AccountingDepartment, ...employee: string[]): void {
+    this.employees.push(
+      ...employee.filter((em) => {
+        return em !== "Max";
+      })
+    );
   }
 
   addReport(report: string) {
-    this.reports.push(report)
+    this.reports.push(report);
   }
 
   printReport() {
     console.log(this.reports);
-    
   }
 }
 
-const accounting = new AccountingDepartment('d2')
-accounting.addEmployee('Carl', 'Segan')
-accounting.addReport('Good')
+const accounting = new AccountingDepartment("d2");
+accounting.addEmployee("Max", "Segan");
+accounting.addReport("Good");
 
-const something = {addEmployee: it.addEmployee}
+const something = { addEmployee: it.addEmployee };
 
 console.log(it);
 console.log(accounting);
-
-
 
 // something.addEmployee('Jerry') // throws error because of 'this' in param, something doesn't have employee proterty
 // console.log(accounting.employees); // throws error because 'employees' is private
