4efdb5978401623bf0d9538b7b1f735421c19258
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-12 20:05:35 +0600

    examples of type guards

--- src/app.ts
+++ src/app.ts
@@ -16,11 +16,79 @@ const e1: elevateEmployee = {
   startDate: new Date(),
 };
 
-console.log('====================================');
+console.log("====================================");
 console.log(e1);
-console.log('====================================');
+console.log("====================================");
 
 type Combinable = string | number;
 type isThere = boolean | number;
 
-type universal = Combinable & isThere
+type universal = Combinable & isThere;
+
+// type guard
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+console.log('====================================');
+console.log(add('Hello ', 'There!'));
+console.log(add(5, 6));
+console.log('====================================');
+
+const showEmployeeDetail = (employee: admin | employee) => {
+  console.log("Name: " + employee.name)
+  if ("privileges" in employee) {
+    console.log("Priviledges: " + employee.privileges);
+  }
+  if ('startDate' in employee)
+  {
+    console.log("Start Date: " + employee.startDate);
+  }
+}
+
+showEmployeeDetail(e1);
+const e2: employee = {
+  name: 'Rabbi',
+  startDate: new Date()
+}
+showEmployeeDetail(e2)
+
+class Car {
+  move() {
+    console.log('drive around');
+    
+  }
+}
+
+class Truck {
+  move() {
+    console.log('driving around in truck...');
+    
+  }
+
+  load(amount: number) {
+    console.log('Loaded in cargo: ' + amount);
+    
+  }
+}
+
+type Vehicle = Car | Truck
+
+let bumblebee: Vehicle;
+bumblebee = new Car()
+
+function callVehicle(vehicle: Vehicle) {
+  vehicle.move();
+  if (vehicle instanceof Truck)
+  {
+    vehicle.load(500);
+  }
+}
+
+callVehicle(bumblebee)
+
+
