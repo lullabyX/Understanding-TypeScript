30e1388ae61514de2e390ae1864cf89147d6ca6c
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-12 18:01:04 +0600

    implement interface with class

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,33 @@
-interface Greetable {
-  name: string;
-
-  greeting(phrase: string): void;
+interface Named {
+  readonly name: string;
 }
 
-let user1: Greetable;
+interface Greetable extends Named {
+  greeting: (phrase: string) => void
+}
 
-user1 = {
-  name: "Hassan",
+class User implements Greetable {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
   greeting(phrase: string) {
-    console.log("====================================");
-    console.log(phrase + " I'm " + this.name);
-    console.log("====================================");
-  },
-};
+    console.log('====================================');
+    console.log(phrase + ', I\'m ' + this.name);
+    console.log('====================================');
+  }
+}
 
-user1.greeting('Hello there, ')
+let user1: Greetable; // this tells, you don't care whether user1 is of class User but ensures
+                      // user1 is Greetable. This works becasue class User implements Greetable
+
+const user2 = new User('Rabbi', 25);
+
+user1 = new User('Hassan', 30);
+
+user1.greeting('Hello there')
+
+user2.greeting('Hi')
