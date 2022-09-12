fb24a0f87dd24b3869f5be9c3f742c8e2b96f5b3
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-12 21:55:39 +0600

    nullish coalescing

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,8 @@ const fetchedData = {
 
 console.log(fetchedData.job?.title);
 
+const userName = null;
+const admin = userName ?? 'DarthVader69' // ?? is the nullish coalescing, assigns fallback default if
+                                          // assignment variable is null or undefined
+                                          // || ignores empty string
+console.log(admin);
