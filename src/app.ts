c1f50533f07d17d671d4397ceb8e996cc441e2a4
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-12 19:47:29 +0600

    intersection types

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
+type admin = {
+  name: string;
+  privileges: string[];
+};
+
+type employee = {
+  name: string;
+  startDate: Date;
+};
+
+type elevateEmployee = admin & employee;
+
+const e1: elevateEmployee = {
+  name: "Hassan",
+  privileges: ["create-server", "manage-server"],
+  startDate: new Date(),
+};
+
 console.log('====================================');
-console.log('Trying out typescript');
+console.log(e1);
 console.log('====================================');
+
+type Combinable = string | number;
+type isThere = boolean | number;
+
+type universal = Combinable & isThere
