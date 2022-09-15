bd6320df1a33bcd0017729331c333615ae4b6175
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-15 18:17:16 +0600

    decorator execution order

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,43 @@
 function Logger(constructor: Function) {
   console.log('Decorator: Logging the class');
   console.log(constructor);
-  
 }
 
-function Factory(element: string, hookId: string) {
+function Header(element: string, hookId: string) {
+  console.log('Header decorator factory called...');
+  
   return (_: Function) => {
+    console.log("Header Logging...");
+    
     const hookEl = document.getElementById(hookId)!;
     hookEl.innerHTML = element
   }
 }
 
+function CarHeader(hookId: string) {
+  console.log("CarHeader decorator factory called...");
+  return (constructor: any) => {
+    console.log("CarHeader logging...");
+
+    const p = new constructor("Karin Kuruma");
+    const hookEl = document.getElementById(hookId)!;
+    hookEl.textContent = p.name;
+  };
+}
+
 @Logger
-@Factory('<h1>This is a header</h1>', 'app')
+@Header("<h1>This is a header</h1>", "app")
+@CarHeader('another')
 class Car {
   name: string;
-  constructor (t: string) {
-    console.log('Initializing a car instance...');
-    
+  constructor(t: string) {
+    console.log("Initializing a car instance...");
+
     this.name = t;
   }
 
   printName() {
     console.log(`name is ${this.name}`);
-    
   }
 }
 
