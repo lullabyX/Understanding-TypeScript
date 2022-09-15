ed6bcdeee4f17b50366582b7b06d5b208974f479
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-15 15:16:36 +0600

    generic class

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,38 @@
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return `${String(key)} are ${obj[key]}`;
+class DataStorage<T extends string | number | boolean>{
+  private data: T[] = []
+
+  addItem(this: DataStorage<T>, item: T) {
+    this.data.push(item)
+  }
+
+  removeItem(this: DataStorage<T>, item: T) {
+    // this.data = this.data.filter(d => d != item)
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getLength(this: DataStorage<T>) {
+    return this.data.length;
+  }
+
+  printItems(this: DataStorage<T>) {
+    console.log(this.data);
+  }
 }
 
-const torkari = {
-  masala: ["holud, morich, peyaj"],
-  sabji: ["alu", "potol"],
-  "non-veg": ["murgi", "dim"],
-  $_$: 'prochur taka'
-};
+const stringStorage = new DataStorage<string>();
 
-let extractedValue = extractAndConvert(torkari, 'masala')
+stringStorage.addItem('hello');
+stringStorage.addItem("there");
+stringStorage.addItem("general");
+stringStorage.addItem("kenobi");
+stringStorage.removeItem("kenobi")
+stringStorage.addItem("Kenobi")
+stringStorage.printItems()
 
-console.log(extractedValue);
+const numberStorage = new DataStorage<number>()
+numberStorage.addItem(54);
+numberStorage.addItem(34);
+numberStorage.printItems();
+
 
 
