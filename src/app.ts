a81c05cc3f34525d55edd591110bf6b0e39132fc
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-15 14:03:10 +0600

    built in generics

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
-console.log('====================================');
-console.log('Trying out typescript');
-console.log('====================================');
+let myArray: Array<string>;
+
+myArray = ["hello"];
+
+myArray.push("43");
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("The promise is resolved");
+  }, 2000);
+});
+
+promise.then((data) => {
+  console.log(data.split(' '));
+  
+});
