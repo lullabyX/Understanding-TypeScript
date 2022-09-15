3a7b855ea468954cf14204631c438e7392893657
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-15 15:34:17 +0600

    utility types Partial & Readonly

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,23 @@
-class DataStorage<T extends string | number | boolean>{
-  private data: T[] = []
+interface CourseGoal {
+  title: string;
+  description: string;
+  date: Date;
+}
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
 
-  addItem(this: DataStorage<T>, item: T) {
-    this.data.push(item)
-  }
-
-  removeItem(this: DataStorage<T>, item: T) {
-    // this.data = this.data.filter(d => d != item)
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getLength(this: DataStorage<T>) {
-    return this.data.length;
-  }
-
-  printItems(this: DataStorage<T>) {
-    console.log(this.data);
-  }
+  let courseGoal: Partial<CourseGoal> = {}
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.date = date;
+  return courseGoal as CourseGoal;
 }
 
-const stringStorage = new DataStorage<string>();
+console.log(createCourseGoal('TS', 'Understand the basics', new Date()));
 
-stringStorage.addItem('hello');
-stringStorage.addItem("there");
-stringStorage.addItem("general");
-stringStorage.addItem("kenobi");
-stringStorage.removeItem("kenobi")
-stringStorage.addItem("Kenobi")
-stringStorage.printItems()
+const admins: Readonly<string[]> = ['Hassan', 'Rabbi']
 
-const numberStorage = new DataStorage<number>()
-numberStorage.addItem(54);
-numberStorage.addItem(34);
-numberStorage.printItems();
+// admins.push()  // throws error
+// admins.pop() // throws error
 
-
+console.log(admins);
 
