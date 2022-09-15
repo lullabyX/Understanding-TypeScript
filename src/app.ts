d158005ef4fa78f2adfa964f7400371ed1525afb
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-15 14:17:44 +0600

    generic function & constrains

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,11 @@
-let myArray: Array<string>;
+function mergeObject<T extends object, U>(obj1: T, obj2: U){
+  // return {...obj1, ...obj2}
+  return Object.assign(obj1, obj2) // it will throw error if constrained on Object.assign first param
+                                  // is not object
+}
 
-myArray = ["hello"];
+let combined = mergeObject({ firstName: "hassan", age: 30, hobbies: ['games'] }, { lastName: "rabbi" });
 
-myArray.push("43");
+// console.log(combined.firstName); // throws error if no function generic is set
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("The promise is resolved");
-  }, 2000);
-});
-
-promise.then((data) => {
-  console.log(data.split(' '));
-  
-});
+console.log(combined);
