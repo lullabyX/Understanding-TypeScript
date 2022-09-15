6f550d707435b5b92835ee3ea45462d9d129390f
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-15 18:02:16 +0600

    class decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
-console.log('====================================');
-console.log('Trying out typescript');
-console.log('====================================');
+function Logger(constructor: Function) {
+  console.log('Decorator: Logging the class');
+  console.log(constructor);
+  
+}
+
+@Logger
+class Car {
+  name: string;
+  constructor (t: string) {
+    console.log('Initializing a car instance...');
+    
+    this.name = t;
+  }
+
+  printName() {
+    console.log(`name is ${this.name}`);
+    
+  }
+}
+
+const car1 = new Car('Kuruma');
+car1.printName();
