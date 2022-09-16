5bf3491a850b64e84166ac3a4d6014a96615e46b
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-16 18:09:55 +0600

    validation with decorators

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ function Logger(constructor: Function) {
 
 function CarHeader(element: string, hookId: string) {
   console.log("CarHeader decorator factory called...");
-  return function <T extends { new ( ...args: any[]): {name:string} }>(
+  return function <T extends { new (...args: any[]): { name: string } }>(
     originalConstructor: T
   ) {
     return class extends originalConstructor {
@@ -50,7 +50,7 @@ class Car {
   @PropertyDecorator // gets the constructor of the class as target
   static type = "Japanese";
   @PropertyDecorator // gets the prototype of the class as target
-  name = 'Kuruma';
+  name = "Kuruma";
   private _price: number;
   constructor(t: string, p: number) {
     console.log("Initializing a car instance...");
@@ -73,23 +73,26 @@ class Car {
   }
 }
 
-const car1 = new Car( 'Karin', 10000);
+const car1 = new Car("Karin", 10000);
 car1.printName();
 
-
-function Autobind(_: any, _2: string | Symbol | number, descriptor: PropertyDescriptor):PropertyDescriptor {
+function Autobind(
+  _: any,
+  _2: string | Symbol | number,
+  descriptor: PropertyDescriptor
+): PropertyDescriptor {
   const originalMethod = descriptor.value;
   return {
     configurable: true,
     enumerable: false,
     get() {
-      return originalMethod.bind(this)
-    }
-  }
+      return originalMethod.bind(this);
+    },
+  };
 }
 
 class Printer {
-  message = 'This works!';
+  message = "This works!";
 
   @Autobind
   showMessage() {
@@ -99,8 +102,87 @@ class Printer {
 
 const p = new Printer();
 
-const clickMe = document.getElementById('button');
+const clickMe = document.getElementById("button");
 
-clickMe?.addEventListener('click', p.showMessage)
+clickMe?.addEventListener("click", p.showMessage);
 
+interface ValidatorConfig {
+  [constructorName: string]: {
+    [prop: string]: string[];
+  };
+}
 
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, name: string) {
+  const constructorName = target.constructor.name;
+  registeredValidators[constructorName] = {
+    ...registeredValidators[constructorName],
+    [name]: [...registeredValidators.constructorName?.name ?? [], "required"],
+  };
+}
+
+function Positive(target: any, name: string) {
+  const constructorName = target.constructor.name;
+  registeredValidators[constructorName] = {
+    ...registeredValidators[constructorName],
+    [name]: [...registeredValidators.constructorName?.name ?? [], "positive"],
+  };
+}
+
+function Validate(obj: any): boolean {
+  let isValid = true;
+  const objValidatorConfig = registeredValidators[obj.constructor.name]
+  
+  if (!objValidatorConfig)
+    return isValid;
+  for (const prop in objValidatorConfig)
+  {
+    for (const validator of objValidatorConfig[prop])
+    { 
+      switch (validator)
+      {
+        case 'required':
+          isValid = isValid && !!obj[prop]
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid
+}
+
+class Course {
+  @Required
+  name: string;
+  @Positive
+  price: number;
+
+  constructor(n: string, p: number) {
+    this.name = n;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')
+
+courseForm?.addEventListener('submit', event => {
+  event.preventDefault();
+  const courseName = document.getElementById('name') as HTMLInputElement;
+  const coursePrice = document.getElementById('price') as HTMLInputElement;
+
+  const name = courseName.value;
+  const price = +coursePrice.value;
+
+  const createdCourse = new Course(name, price);
+
+  if (!Validate(createdCourse))
+  {
+    alert('Invalid Input, please try again!')
+    return;
+  }
+  console.log(createdCourse);
+  
+})
