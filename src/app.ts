cd9c6126c356fe3b71159916446f6a3a403cd3ff
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-16 17:07:33 +0600

    changing class constructor with decorator

--- src/app.ts
+++ src/app.ts
@@ -1,71 +1,67 @@
 function Logger(constructor: Function) {
-  console.log('Decorator: Logging the class');
+  console.log("Decorator: Logging the class");
   console.log(constructor);
 }
 
-function Header(element: string, hookId: string) {
-  console.log('Header decorator factory called...');
-  
-  return (_: Function) => {
-    console.log("Header Logging...");
-    
-    const hookEl = document.getElementById(hookId)!;
-    hookEl.innerHTML = element
-  }
-}
-
-function CarHeader(hookId: string) {
+function CarHeader(element: string, hookId: string) {
   console.log("CarHeader decorator factory called...");
-  return (constructor: any) => {
-    console.log("CarHeader logging...");
-
-    const p = new constructor("Karin Kuruma", 10000);
-    const hookEl = document.getElementById(hookId)!;
-    hookEl.textContent = p.name;
+  return function <T extends { new ( ...args: any[]): {name:string} }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(...args: any[]) {
+        super(...args);
+        console.log("CarHeader logging...");
+        const hookEl = document.getElementById(hookId)!;
+        hookEl.innerHTML = element;
+        hookEl.querySelector("h1")!.textContent = this.name;
+      }
+    };
   };
 }
 
 function PropertyDecorator(target: any, name: string) {
-  console.log('Property Decorator...');
+  console.log("Property Decorator...");
   console.log(target);
   console.log(name);
-  
 }
 
-function AccessorDecorator(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log('Accessor Decorator');
+function AccessorDecorator(
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Accessor Decorator");
   console.log(target);
   console.log(name);
   console.log(descriptor);
-  
 }
 
 function ParameterDecorator(target: any, name: string, position: number) {
-  console.log('Parameter Decorator...');
+  console.log("Parameter Decorator...");
   console.log(target);
   console.log(name);
   console.log(position);
 }
 
 @Logger
-@Header("<h1>This is a header</h1>", "app")
-@CarHeader('another')
+@CarHeader("<h1>This is a header</h1>", "app")
 class Car {
   @PropertyDecorator // gets the constructor of the class as target
-  static type = 'Japanese'
+  static type = "Japanese";
   @PropertyDecorator // gets the prototype of the class as target
-  name: string;
-  private _price: number
+  name = 'Kuruma';
+  private _price: number;
   constructor(t: string, p: number) {
     console.log("Initializing a car instance...");
 
     this.name = t;
-    this._price = p
+    this._price = p;
   }
 
   @AccessorDecorator
   get price() {
-    return this._price
+    return this._price;
   }
 
   printName() {
@@ -73,9 +69,9 @@ class Car {
   }
 
   calculateTax(@ParameterDecorator tax: number) {
-    return this._price * (1+tax)
+    return this._price * (1 + tax);
   }
 }
 
-const car1 = new Car('Kuruma', 10000);
+const car1 = new Car( 'Karin', 10000);
 car1.printName();
